abc3878b0634572c6bfdfc871f2a9cab3a9aea6a
Garret Wassermann <gwasser@gmail.com>
2024-10-14 20:46:46 -0400

    don't know what happened here, copy paste error

--- bootstrap5form/meta.py
+++ bootstrap5form/meta.py
@@ -3,9 +3,7 @@ from packaging.version import Version, parse
 from platform import python_version
 
 if Version(python_version()) >= Version('3.10'):
-VERSION = StrictVersion('5.0.2')
     VERSION = Version('5.0.2')
-
 else:
     from distutils.version import StrictVersion
     VERSION = StrictVersion('5.0.2')
